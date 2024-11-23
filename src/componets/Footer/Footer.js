fd2adeb366a3a8d9fc2c2bb994aae6efcadca5d1
nadushanik <nosonova.nadegda@yandex.ru>
2024-11-23 16:23:18 +0700

    Add files via upload

---
+++ src/componets/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Filter from '../Filter'
+import './Footer.css'
+
+const Footer = ({
+  activeTasksCount,
+  filter,
+  onFilterChange,
+  clearCompleted,
+}) => {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{activeTasksCount} items left</span>
+      <Filter filter={filter} onFilterChange={onFilterChange} />
+      <button className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  )
+}
+
+Footer.defaultProps = {
+  activeTasksCount: 0,
+  filter: 'all',
+  onFilterChange: () => {},
+  clearCompleted: () => {},
+}
+
+Footer.propTypes = {
+  activeTasksCount: PropTypes.number,
+  filter: PropTypes.string,
+  onFilterChange: PropTypes.func,
+  clearCompleted: PropTypes.func,
+}
+
+export default Footer
