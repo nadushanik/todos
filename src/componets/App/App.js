57d5d3487e406093da71908d485aed412d82985a
nadushanik <nosonova.nadegda@yandex.ru>
2024-11-23 16:51:59 +0700

    Update App.js

--- src/componets/App/App.js
+++ src/componets/App/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import AppHeader from '../AppHeader'
-import NewTaskForm from '../NewTaskForm'
-import TaskList from '../TaskList'
-import Footer from '../Footer'
+import AppHeader from "../AppHeader";
+import NewTaskForm from "../NewTaskForm";
+import TaskList from "../TaskList";
+import Footer from "../Footer";
 
-import './App.css'
+import "./App.css";
 
 export default class App extends Component {
-  maxId = 3
+  maxId = 3;
   state = {
-    todoData: [this.createTask('drink coffee', 1, 0)],
-    filter: 'all',
+    todoData: [this.createTask("drink coffee", 1, 0)],
+    filter: "all",
     editingTask: null,
-  }
+  };
 
   static defaultProps = {
     todoData: [
@@ -23,12 +23,12 @@ export default class App extends Component {
         completed: false,
       },
     ],
-    filter: 'all',
-  }
+    filter: "all",
+  };
 
   static propTypes = {
     todoData: PropTypes.instanceOf(Array),
-  }
+  };
 
   createTask(label, minutes, seconds) {
     return {
@@ -38,89 +38,89 @@ export default class App extends Component {
       createdAt: new Date(),
       minutes,
       seconds,
-    }
+    };
   }
 
   delitTask = (id) => {
     this.setState(({ todoData }) => {
-      const newArray = todoData.filter((el) => el.id !== id)
+      const newArray = todoData.filter((el) => el.id !== id);
       return {
         todoData: newArray,
-      }
-    })
-  }
+      };
+    });
+  };
 
   toggleCompleted = (id) => {
     this.setState(({ todoData }) => {
       const newArray = todoData.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-      )
+      );
       return {
         todoData: newArray,
-      }
-    })
-  }
+      };
+    });
+  };
 
   addTask = (text, minutes, seconds) => {
-    const newTask = this.createTask(text, minutes, seconds)
+    const newTask = this.createTask(text, minutes, seconds);
     this.setState(({ todoData }) => {
-      const newArr = [...todoData, newTask]
+      const newArr = [...todoData, newTask];
       return {
         todoData: newArr,
-      }
-    })
-  }
+      };
+    });
+  };
 
   clearCompleted = () => {
     this.setState(({ todoData }) => {
-      const newDataStream = todoData.filter((el) => el.completed === false)
+      const newDataStream = todoData.filter((el) => el.completed === false);
       return {
         todoData: newDataStream,
-      }
-    })
-  }
+      };
+    });
+  };
 
   filter(items, filter) {
     switch (filter) {
-      case 'all':
-        return items
-      case 'active':
-        return items.filter((item) => !item.completed)
-      case 'completed':
-        return items.filter((item) => item.completed)
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.completed);
+      case "completed":
+        return items.filter((item) => item.completed);
       default:
-        return items
+        return items;
     }
   }
 
   search(items) {
-    return items
+    return items;
   }
 
   onFilterChange = (filter) => {
-    this.setState({ filter })
-  }
+    this.setState({ filter });
+  };
 
   startEditing = (id) => {
-    this.setState({ editingTask: id })
-  }
+    this.setState({ editingTask: id });
+  };
 
   saveEditing = (id, newText) => {
     this.setState(({ todoData }) => {
       const newDataStream = todoData.map((item) =>
         item.id === id ? { ...item, label: newText } : item,
-      )
+      );
       return {
         todoData: newDataStream,
         editingTask: null,
-      }
-    })
-  }
+      };
+    });
+  };
 
   render() {
-    const { todoData, filter, editingTask } = this.state
-    const activeTasksCount = todoData.filter((task) => !task.completed).length
-    const visibleTask = this.filter(this.search(todoData), filter)
+    const { todoData, filter, editingTask } = this.state;
+    const activeTasksCount = todoData.filter((task) => !task.completed).length;
+    const visibleTask = this.filter(this.search(todoData), filter);
     return (
       <section className="todoapp">
         <AppHeader />
@@ -142,6 +142,6 @@ export default class App extends Component {
           />
         </section>
       </section>
-    )
+    );
   }
 }
